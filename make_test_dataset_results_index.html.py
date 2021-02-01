47970f8c51354206e8cfb56e5ab9bf07019d1776
Denis Sumin <da.sumin@gmail.com>
2021-02-01 22:04:10 +0300

    Test dataset scripts

--- make_test_dataset_results_index.html.py
+++ make_test_dataset_results_index.html.py
@@ -20,7 +20,7 @@ with open(os.path.join(input_path, "results_index.html"), "w") as f:
     # f.write(f"</tr>" + "\n")
     for filename in results_list:
         f.write(f"<tr>" + "\n")
-        f.write(f"<td><img src='test_pose_src/{filename.replace('.jpg', '_0_rendered.png')}'></td>" + "\n")
+        f.write(f"<td><img src='test_pose_src/{filename.replace('.jpg', '_rendered.png')}'></td>" + "\n")
         for result in results_dirs:
             image = imageio.imread(os.path.join(input_path, result, filename))
             image_segm = image[:, one_image_width * 0:one_image_width * 1]
@@ -31,10 +31,10 @@ with open(os.path.join(input_path, "results_index.html"), "w") as f:
             imageio.imwrite(os.path.join(input_path, result, filename + "_warped_mask.jpg"), image_warped_mask)
             imageio.imwrite(os.path.join(input_path, result, filename + "_cloth.jpg"), image_cloth)
             imageio.imwrite(os.path.join(input_path, result, filename + "_gen.jpg"), image_gen)
-            f.write(f"<td><img src='test_reference_crop/{filename}_segm.jpg'></td>" + "\n")
-            f.write(f"<td><img src='test_reference_crop/{filename}_warped_mask.jpg'></td>" + "\n")
-            f.write(f"<td><img src='test_reference_crop/{filename}_cloth.jpg'></td>" + "\n")
-            f.write(f"<td><img src='test_reference_crop/{filename}_gen.jpg'></td>" + "\n")
+            f.write(f"<td><img src='{result}/{filename}_segm.jpg'></td>" + "\n")
+            f.write(f"<td><img src='{result}/{filename}_warped_mask.jpg'></td>" + "\n")
+            f.write(f"<td><img src='{result}/{filename}_cloth.jpg'></td>" + "\n")
+            f.write(f"<td><img src='{result}/{filename}_gen.jpg'></td>" + "\n")
         f.write(f"<td><img src='test_reference_crop/{filename}'></td>" + "\n")
         f.write(f"</tr>" + "\n")
     f.write(f"</table>" + "\n")
