7640e5f544651fa7047ee543f4150e704d7b437b
Denis Sumin <da.sumin@gmail.com>
2021-02-02 18:06:56 +0300

    Add collection of reference model/cloth photos in make_test_dataset.py

--- make_test_dataset.py
+++ make_test_dataset.py
@@ -4,6 +4,7 @@ import sys
 
 import cv2
 import imageio
+import numpy
 
 input_path = sys.argv[1]
 output_path = sys.argv[2]
@@ -38,33 +39,43 @@ for dirname in sorted(os.listdir(input_path)):
     reference_model_img_crop = reference_model_img[:256, (w-192) // 2:-(w-192) // 2]
 
     reference_model_photo_dir = os.path.join(photos_path, "tops", model_id + "_0", model_id)
-    reference_model_photo_filename = sorted(os.listdir(reference_model_photo_dir))[1]
-    reference_model_photo = imageio.imread(os.path.join(
-        reference_model_photo_dir, reference_model_photo_filename
-    ))
-    reference_model_photo = cv2.resize(reference_model_photo, dsize=None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA)
-    h, w = reference_model_photo.shape[:2]
-    reference_model_photo_crop = reference_model_photo[:256, (w - 192) // 2:-(w - 192) // 2]
+    try:
+        reference_model_photo_filename = sorted(os.listdir(reference_model_photo_dir))[1]
+    except FileNotFoundError:
+        print(f"{reference_model_photo_dir} does not exist")
+        reference_model_photo_crop = numpy.ones(shape=(256, 192, 3), dtyle=numpy.uint8) * 200
+    else:
+        reference_model_photo = imageio.imread(os.path.join(
+            reference_model_photo_dir, reference_model_photo_filename
+        ))
+        reference_model_photo = cv2.resize(reference_model_photo, dsize=None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA)
+        h, w = reference_model_photo.shape[:2]
+        reference_model_photo_crop = reference_model_photo[:256, (w - 192) // 2:-(w - 192) // 2]
 
     for test in tests:
         imageio.imwrite(os.path.join(output_path, "test_img_ref", f"{test_idx}.jpg"), reference_model_img, quality=97)
         imageio.imwrite(os.path.join(output_path, "test_img", f"{test_idx}.jpg"), reference_model_img_crop, quality=97)
 
-        with open(os.path.join(input_path, dirname, reference, "meta.json"), "r") as f:
+        imageio.imwrite(os.path.join(output_path, "test_photo_model", f"{test_idx}.jpg"), reference_model_photo_crop, quality=97)
+
+        with open(os.path.join(input_path, dirname, test, "meta.json"), "r") as f:
             reference_meta = json.load(f)
         top_product_id = dict(zip(reference_meta["product_categories"], reference_meta["product_ids"]))["tops"]
 
         reference_cloth_photo_dir = os.path.join(photos_path, "tops", top_product_id, top_product_id[:-2])
-        reference_cloth_photo_filename = sorted(os.listdir(reference_cloth_photo_dir))[1]
-        reference_cloth_photo = imageio.imread(os.path.join(
-            reference_cloth_photo_dir, reference_cloth_photo_filename
-        ))
-        reference_cloth_photo = cv2.resize(reference_cloth_photo, dsize=None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA)
-        h, w = reference_cloth_photo.shape[:2]
-        reference_cloth_photo_crop = reference_model_photo[:256, (w - 192) // 2:-(w - 192) // 2]
+        try:
+            reference_cloth_photo_filename = sorted(os.listdir(reference_cloth_photo_dir))[1]
+        except FileNotFoundError:
+            print(f"{reference_model_photo_dir} does not exist")
+            reference_cloth_photo_crop = numpy.ones(shape=(256, 192, 3), dtyle=numpy.uint8) * 200
+        else:
+            reference_cloth_photo = imageio.imread(os.path.join(
+                reference_cloth_photo_dir, reference_cloth_photo_filename
+            ))
+            reference_cloth_photo = cv2.resize(reference_cloth_photo, dsize=None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA)
+            h, w = reference_cloth_photo.shape[:2]
+            reference_cloth_photo_crop = reference_model_photo[:256, (w - 192) // 2:-(w - 192) // 2]
         imageio.imwrite(os.path.join(output_path, "test_photo_cloth", f"{test_idx}.jpg"), reference_cloth_photo_crop, quality=97)
-
-        imageio.imwrite(os.path.join(output_path, "test_photo_model", f"{test_idx}.jpg"), reference_model_photo_crop, quality=97)
 
         img = imageio.imread(os.path.join(input_path, dirname, test, "tops.jpg"))
         img = cv2.resize(img, dsize=(192, 256), interpolation=cv2.INTER_AREA)
