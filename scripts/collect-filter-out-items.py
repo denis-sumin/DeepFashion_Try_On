badd0ba6cc3fe4ae363a64af426c4bfe5064ee33
Denis Sumin <da.sumin@gmail.com>
2021-03-03 00:27:00 +0300

    Add checks to collect-filter-out-items.py

--- scripts/collect-filter-out-items.py
+++ scripts/collect-filter-out-items.py
@@ -25,7 +25,7 @@ with open(ce_losses_ids_file, "r") as f:
 checked_ids = set()
 filtered_ids = {}
 
-for filename in os.listdir(input_xlsx_dir):
+for filename in sorted(os.listdir(input_xlsx_dir)):
     if not filename.lower().endswith(".xlsx"):
         print(f"Skipping {filename}")
         continue
@@ -36,6 +36,8 @@ for filename in os.listdir(input_xlsx_dir):
     except ValueError:
         print(f"Skipping {filename}")
         continue
+
+    print(filename)
     checked_ids_this_file = set(
         (item["id"] for item in ce_losses[file_page_start:file_page_start+1000])
     )
@@ -46,16 +48,27 @@ for filename in os.listdir(input_xlsx_dir):
     filtered_ids_this_file = {}
     for key, _, pose_name, _ in checkbox_names:
         filtered_set = set(check_df[pose_name].dropna().astype("int").values)
+
+        if not filtered_set.issubset(checked_ids_this_file):
+            print("These ids could not be present in this check file",
+                  filtered_set.difference(checked_ids_this_file))
+            filtered_set -= filtered_set.difference(checked_ids_this_file)
+
         filtered_ids_this_file[key] = filtered_set
 
         filtered_ids[key] = filtered_ids.get(key, set()).union(filtered_set)
 
+    filtered_ids_this_file_union = {
+        item for values in filtered_ids_this_file.values() for item in values
+    }
+
 filtered_ids_union = {item for values in filtered_ids.values() for item in values}
-print(len(filtered_ids_union))
 for key, values in filtered_ids.items():
     print(key, len(values))
 
+print(len(filtered_ids_union))
 print(len(checked_ids))
+print(len(checked_ids - filtered_ids_union))
 
 dataset_keys = {item["id"]: item["key"] for item in ce_losses}
 
