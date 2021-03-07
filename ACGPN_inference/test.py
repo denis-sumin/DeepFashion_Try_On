7298852dcb6f7200063bff49aad2b839296f47cb
Denis Sumin <da.sumin@gmail.com>
2021-03-07 20:09:56 +0300

    Write images and metrics during test inference

--- ACGPN_inference/test.py
+++ ACGPN_inference/test.py
@@ -2,6 +2,7 @@ import os
 import time
 
 import cv2
+import imageio
 import numpy as np
 import torch
 import util.util as util
@@ -15,6 +16,8 @@ from data.data_loader import CreateDataLoader
 writer = SummaryWriter("runs/G1G2")
 SIZE = 320
 NC = 14
+
+opt = TrainOptions().parse()
 
 
 def generate_label_plain(inputs):
@@ -32,10 +35,10 @@ def generate_label_plain(inputs):
     return label_batch
 
 
-def generate_label_color(inputs):
+def generate_label_color(inputs, num_labels=opt.label_nc):
     label_batch = []
     for i in range(len(inputs)):
-        label_batch.append(util.tensor2label(inputs[i], opt.label_nc))
+        label_batch.append(util.tensor2label(inputs[i], num_labels))
     label_batch = np.array(label_batch)
     label_batch = label_batch * 2 - 1
     input_label = torch.from_numpy(label_batch)
@@ -75,7 +78,6 @@ def changearm(old_label):
     return label
 
 
-opt = TrainOptions().parse()
 os.makedirs(opt.results_dir, exist_ok=True)
 iter_path = os.path.join(opt.checkpoints_dir, opt.name, "iter.txt")
 if opt.continue_train:
@@ -108,6 +110,8 @@ print_delta = total_steps % opt.print_freq
 save_delta = total_steps % opt.save_latest_freq
 
 step = 0
+
+losses_mapping = {}
 
 for epoch in range(start_epoch, opt.niter + opt.niter_decay + 1):
     epoch_start_time = time.time()
@@ -205,20 +209,115 @@ for epoch in range(start_epoch, opt.niter + opt.niter_decay + 1):
 
         ############## Display results and errors ##########
 
+        def make_vis(tensor):
+            image = tensor.squeeze().numpy().copy()
+            if len(image.shape) == 3:
+                image = np.moveaxis(image, 0, -1)
+            # if not put_palette:
+            #     image = (image + 1.) / 2. * 255
+            return image.astype(np.uint8)
+
+
+        G1_in_cpu = G1_in.detach().cpu()
+        G1_in_cloth_mask = G1_in_cpu[:, 0]
+        G1_in_cloth_image = G1_in_cpu[:, 1:4]
+        G1_in_label = G1_in_cpu[:, 4:4 + 14]
+        G1_in_pose = G1_in_cpu[:, 4 + 14:-1]
+        G1_in_noise = G1_in_cpu[:, -1]
+
+        G1_in_label_vis = generate_label_color(generate_label_plain(G1_in_label))
+        G1_in_pose_vis = generate_label_color(
+            generate_discrete_label(G1_in_pose, G1_in_pose.shape[1], False),
+            G1_in_pose.shape[1]
+        )
+
+        G2_in_cpu = G2_in.detach().cpu()
+        G2_out_cpu = G2_out.detach().cpu()
+
+        G2_in_cloth_mask = G2_in_cpu[:, 0]
+        G2_in_cloth_image = G2_in_cpu[:, 1:4]
+        G2_in_label = G2_in_cpu[:, 4:4 + 14]
+        G2_in_pose = G2_in_cpu[:, 4 + 14:-1]
+        G2_in_noise = G2_in_cpu[:, -1]
+
+        Unet_clothes_cpu = Unet_clothes.cpu()
+        Unet_clothes_mask_cpu = Unet_clothes_mask.cpu()
+        Unet_pre_clothes_mask_cpu = Unet_pre_clothes_mask.cpu()
+        Unet_grid_cpu = Unet_grid.cpu()
+
+        fake_c_cpu = fake_c.detach().cpu()
+        warped_cpu = warped.detach().cpu()
+        warped_mask_cpu = warped_mask.detach().cpu()
+        warped_grid_cpu = warped_grid.detach().cpu()
+
+        G_in_cpu = G_in.detach().cpu()
+        G_out_cpu = G_out.detach().cpu()
+
+        G_in_img_hole_hand = G_in_cpu[:, 3]
+        G_in_masked_label = G_in_cpu[:, 3:3 + 14]
+        G_in_image = G_in_cpu[:, 3 + 14:3 + 14 + 3]
+        G_in_skin_color = G_in_cpu[:, 3 + 14 + 3:-1]
+        G_in_noise = G_in_cpu[:, -1]
+
+        output_vis_mapping = {
+            "G1_in_cloth_mask": G1_in_cloth_mask,
+            "G1_in_cloth_image": G1_in_cloth_image,
+            "G1_in_label_vis": G1_in_label_vis,
+            "G1_in_pose_vis": G1_in_pose_vis,
+            "G1_in_noise": G1_in_noise,
+
+            "G1_out": generate_label_color(generate_label_plain(input_label)),
+            "G1_gt": generate_label_color((data["label"] * (1 - mask_clothes))),
+
+            "G2_in_cloth_mask": G2_in_cloth_mask,
+            "G2_in_cloth_image": G2_in_cloth_image,
+            "G2_in_label": generate_label_color(generate_label_plain(G2_in_label)),
+            "G2_in_pose": generate_label_color(
+                generate_discrete_label(G2_in_pose, G2_in_pose.shape[1], False),
+                G2_in_pose.shape[1]
+            ),
+            "G2_in_noise": G2_in_noise,
+
+            "G2_out": G2_out_cpu,
+            "G2_gt": mask_clothes,
+
+            "Unet_clothes": Unet_clothes_cpu,
+            "Unet_clothes_mask": Unet_clothes_mask_cpu,
+            "Unet_pre_clothes_mask": Unet_pre_clothes_mask_cpu,
+            "Unet_grid": Unet_grid_cpu,
+
+            "fake_c": fake_c_cpu,
+            "warped": warped_cpu,
+            "warped_mask": warped_mask_cpu,
+            "warped_grid": warped_grid_cpu,
+
+            "G_in_img_hole_hand": G_in_img_hole_hand,
+            "G_in_masked_label": generate_label_color(generate_label_plain(G_in_masked_label)),
+            "G_in_image": G_in_image,
+            "G_in_skin_color": G_in_skin_color,
+            "G_in_noise": G_in_noise,
+
+            "G_out": G_out_cpu,
+        }
+
+        for vis_name, vis_image in output_vis_mapping.items():
+            output_filepath = os.path.join(opt.results_dir, vis_name + "_" + data["name"][0])
+            imageio.imwrite(output_filepath, vis_image)
+
         ### display output images
-        a = generate_label_color(generate_label_plain(input_label)).float().cuda()
-        b = real_image.float().cuda()
-        c = fake_image.float().cuda()
-        d = torch.cat([clothes_mask, clothes_mask, clothes_mask], 1)
-        combine = torch.cat([a[0], d[0], b[0], c[0], rgb[0]], 2).squeeze()
-        # combine=c[0].squeeze()
-        cv_img = (combine.permute(1, 2, 0).detach().cpu().numpy() + 1) / 2
-        if step % 1 == 0:
-            writer.add_image("combine", (combine.data + 1) / 2.0, step)
-            rgb = (cv_img * 255).astype(np.uint8)
-            bgr = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
-            n = str(step) + ".jpg"
-            cv2.imwrite(opt.results_dir + "/" + data["name"][0], bgr)
+        # a = generate_label_color(generate_label_plain(input_label)).float().cuda()
+        # b = real_image.float().cuda()
+        # c = fake_image.float().cuda()
+        # d = torch.cat([clothes_mask, clothes_mask, clothes_mask], 1)
+        # combine = torch.cat([a[0], d[0], b[0], c[0], rgb[0]], 2).squeeze()
+        # # combine=c[0].squeeze()
+        # cv_img = (combine.permute(1, 2, 0).detach().cpu().numpy() + 1) / 2
+        # if step % 1 == 0:
+        #     writer.add_image("combine", (combine.data + 1) / 2.0, step)
+        #     rgb = (cv_img * 255).astype(np.uint8)
+        #     bgr = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+        #     n = str(step) + ".jpg"
+        #     cv2.imwrite(opt.results_dir + "/" + data["name"][0], bgr)
         step += 1
         print(step, time.time() - epoch_start_time, (time.time() - epoch_start_time) / step)
         ### save latest model
@@ -229,6 +328,9 @@ for epoch in range(start_epoch, opt.niter + opt.niter_decay + 1):
             pass
         if epoch_iter >= dataset_size:
             break
+
+    with open(os.path.join(opt.results_dir, "ce_losses.json"), "w") as f:
+        json.dump(ce_loss_values, f)
 
     # end of epoch
     iter_end_time = time.time()
